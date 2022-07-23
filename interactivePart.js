db10379b5b886b506d64034c95d6db98b15b2a3e
chloeBarraza <tinsley.chloe@gmail.com>
2022-07-22 20:21:13 -0700

    added array to add button

--- interactivePart.js
+++ interactivePart.js
@@ -1,6 +1,29 @@
-function addElement() {
+// Without window.onload, the script may run before the DOM loads
+window.onload = function () {
+  // Array of to-do items
   let theList = [];
+
+  // Get elements
   const newListItem = document.getElementById("newItem");
-  theList.push(newListItem);
-  console.log(theList);
-}
+  const addButton = document.getElementById("add");
+
+  // Add on click to add button
+  addButton.onclick = function () {
+    addElement();
+    return false;
+  };
+
+  /**
+   * Alternative syntax for anonymous functions
+   * addButton.click = () => {
+   *  addElement();
+   *  return false;
+   * }
+   */
+
+  function addElement() {
+    theList.push(newListItem.value);
+    console.log(theList);
+  }
+};
+//<3 u Chris
